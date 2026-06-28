02785915f9e3f5677d11e0977add83f70a32b26d
EYABELLI <83172479+EYABELLI@users.noreply.github.com>
2026-06-28 13:02:43 +0100

    V1

---
+++ CSF.py
@@ -0,0 +1,8 @@
+print("CSF_Project_Math")
+def addition (a,b):
+     return a+b
+print ("Addition:", addition(5,3))
+
+def soustraction(a,b):
+     return a-b
+print ("sosutraction:",soustraction(5,3))
